5b49585818297d975c496642d9b1667f7cb9f737
Alaa Radwan <a.radwan@paytabs.com>
2022-05-07 23:21:02 +0200

    update in server crons

--- infrastructure/seeds/seed.go
+++ infrastructure/seeds/seed.go
@@ -12,20 +12,20 @@ func Seed() {
 	newDB = db.NewPostgres()
 	db := newDB.Open()
 	defer db.Close()
-    //seed database only one time
+	//seed database only one time
 	countstatus := 0
 	db.GormDB.Model(&entity.Status{}).Count(&countstatus)
 	if countstatus == 0 {
-	SeedWithStatus(db)
+		SeedWithStatus(db)
 	}
 
-	countMedications := 0 
+	countMedications := 0
 	db.GormDB.Model(&entity.Drone{}).Count(&countMedications)
 	if countMedications == 0 {
 		SeedWithMedications(db)
 	}
 
-    //initialize map with prev data in database
+	//initialize map with prev data in database
 	var drones []entity.Drone
 	var drone entity.Drone
 	db.GormDB.Select("serial_num").Table(drone.TableName()).Find(&drones)
@@ -127,44 +127,66 @@ func SeedWithStatus(db *db.DB) {
 
 //update battery if status is loading or returning
 //NODE:we assume that the drone uses it's battery only when its status is delivering or returning
+//NOTE:we assume that the drone is full battery when it's status is IDLE
 func UpdateDroneBattery(db *db.DB) {
 	// reduce 10% from the battery in status delevering and returning every 1 min
-	if err := db.GormDB.Raw("UPDATE drones SET battery_cap = battery_cap - 10 WHERE status = 4 OR status = 6").Scan(&[]entity.Drone{}).Error ; err != nil {
+	if err := db.GormDB.Raw("UPDATE drones SET battery_cap = battery_cap - 10 WHERE status = 4 OR status = 6").Scan(&[]entity.Drone{}).Error; err != nil {
 		worker.Historylog("error update battery for all drones...")
 	}
 	drones := []entity.Drone{}
-	if err := db.GormDB.Where("status = 4 OR status = 6").Find(&drones).Error ; err != nil {
+	if err := db.GormDB.Where("status = 4 OR status = 6").Find(&drones).Error; err != nil {
 		worker.Historylog("error loading drones for all drones...")
 	}
 	for _, v := range drones {
-		worker.Historylog("Drone with Serial:",v.SerialNum,"Has Battery:",v.BatteryCap,"%")
+		worker.Historylog("Drone with Serial:", v.SerialNum, "Has Battery:", v.BatteryCap, "%")
 	}
-} 
+
+	//recharge the battery if the status is idle
+	if err := db.GormDB.Raw("UPDATE drones SET battery_cap = 100 WHERE status = 1").Scan(&entity.Drone{}).Error; err != nil {
+		worker.Historylog("error update battery for all drones...")
+	}
+}
 
 //update loaded drons to DELIVERING, will run it every 2 mins
-//NODE:we create this cron as simulation for drone delivering functionality 
+//NODE:we create this cron as simulation for drone delivering functionality
 func UpdateDroneToDelevering() {
 	var newDB db.Database
 	newDB = db.NewPostgres()
 	db := newDB.Open()
 	defer db.Close()
-	
-	if err := db.GormDB.Model(&entity.Drone{}).Where("status = 3 ").Update("status",4).Error ; err != nil {
+
+	if err := db.GormDB.Model(&entity.Drone{}).Where("status = 3 ").Update("status", 4).Error; err != nil {
 		worker.Historylog("error update status for all drones with status 3 [loaded]...")
 	}
 }
 
 //update delivering to delivered will run every 5 min
-//NODE:we create this cron as simulation for drone delivering functionality 
+//NODE:we create this cron as simulation for drone delivering functionality
+//NOTE:we assume that while the drone is delievered then its weight is reset to 0 and
+//NOTE:remove all medications loaded to this drone
 func UpdateDroneToDelevered() {
 	var newDB db.Database
 	newDB = db.NewPostgres()
 	db := newDB.Open()
 	defer db.Close()
-	
-	if err := db.GormDB.Model(&entity.Drone{}).Where("status = 4 ").Update("status",5).Error ; err != nil {
+
+	if err := db.GormDB.Model(&entity.Drone{}).Where("status = 4 ").Update("status", 5).Update("weight", 0).Error; err != nil {
 		worker.Historylog("error update status for all drones with status 4 [delivering]...")
 	}
+	if err := db.GormDB.Model(&entity.Drone{}).Where("status = 5 ").Update("weight", 0).Error; err != nil {
+		worker.Historylog("error reset weight for all drones with status 5 [delivered]...")
+	}
+	deleteFromDroneMedications(db)
+}
+
+//delete from droneMedications where status is delivered
+func deleteFromDroneMedications(db *db.DB) {
+	DroneMedication := entity.DroneMedication{}
+	if err := db.GormDB.Raw(`
+	DELETE FROM "droneMedications" WHERE drone_serial IN ( SELECT serial_num FROM drones WHERE status = 5)`).Scan(&DroneMedication).Error; err != nil {
+		worker.Historylog("error delete from droneMedications...")
+	}
+	
 }
 
 //update delivering to delivered will run every 3 min
@@ -173,8 +195,8 @@ func UpdateDroneToReturing() {
 	newDB = db.NewPostgres()
 	db := newDB.Open()
 	defer db.Close()
-	
-	if err := db.GormDB.Model(&entity.Drone{}).Where("status = 5 ").Update("status",6).Error ; err != nil {
+
+	if err := db.GormDB.Model(&entity.Drone{}).Where("status = 5 ").Update("status", 6).Error; err != nil {
 		worker.Historylog("error update status for all drones with status 5 [delivered]...")
 	}
 }
--- infrastructure/worker/api_worker/api_worker.go
+++ infrastructure/worker/api_worker/api_worker.go
@@ -3,7 +3,9 @@ package api_worker
 import (
 	"fmt"
 	"task/infrastructure/seeds"
+	"task/infrastructure/worker"
 	"time"
+
 	"github.com/go-co-op/gocron"
 )
 
@@ -14,20 +16,25 @@ func task() {
 }
 
 func task2() {
+	worker.Historylog("task2 -- ")
 	seeds.UpdateDroneToDelevering()
 }
 func task3() {
+	worker.Historylog("task3 -- ")
+
 	seeds.UpdateDroneToReturing()
 }
 func task4()  {
+	worker.Historylog("task4 -- ")
+
 	seeds.UpdateDroneToDelevered()
 }
 func RunCron() {
 	s1 := gocron.NewScheduler(time.UTC)
-	s1.Every(60).Seconds().Do(task)
-	s1.Every(60*2).Second().Do(task2)
-	s1.Every(60*3).Second().Do(task3)
-	s1.Every(60*5).Second().Do(task4)
+	s1.Every(6).Seconds().Do(task)
+	s1.Every(6).Second().Do(task2)
+	s1.Every(30).Second().Do(task3)
+	s1.Every(30).Second().Do(task4)
 	s1.StartAsync()
 }
 
